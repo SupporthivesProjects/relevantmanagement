4ed83e16c8a9980443ed868e440873e0b1a46bce
Jitendra <nerdcode@Jitendras-MacBook-Air.local>
2026-02-02 00:27:11 +0530

    dhiraj branch

--- includes/header.php
+++ includes/header.php
@@ -12,13 +12,13 @@
     <link rel="stylesheet" href="css/swiper-bundle.min.css">
     <link rel="stylesheet" href="uiframe/css/slick.css">
     <link rel="stylesheet" href="uiframe/css/slick-theme.css">
+    <link rel="stylesheet" href="css/dhiraj.css">
+    <link rel="stylesheet" href="css/dhirajresponsive.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/rstyle.css">
     <link rel="stylesheet" href="css/responsive.css">
     <link rel="stylesheet" href="css/privacy.css">
     <link rel="stylesheet" href="css/rstyle.css">
-    <link rel="stylesheet" href="css/dhiraj.css">
-    <link rel="stylesheet" href="css/dhirajresponsive.css">
     <link rel="stylesheet" href="uiframe/css/font-awesome.min.css">
     <link rel="stylesheet" href="uiframe/css/animate.min.css">
     <link href="uiframe/css/aos.css" rel="stylesheet">
