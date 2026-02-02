8b15298f48acfe1e814112c1193ca1020a196783
Jitendra <nerdcode@Jitendras-MacBook-Air.local>
2026-02-02 11:15:13 +0530

    adding privac

--- includes/header.php
+++ includes/header.php
@@ -19,8 +19,6 @@
     <link rel="stylesheet" href="css/s-style.css">
     <link rel="stylesheet" href="css/rstyle.css">
     <link rel="stylesheet" href="css/responsive.css">
-  
-  
     <link rel="stylesheet" href="uiframe/css/font-awesome.min.css">
     <link rel="stylesheet" href="uiframe/css/animate.min.css">
     <link href="uiframe/css/aos.css" rel="stylesheet">
