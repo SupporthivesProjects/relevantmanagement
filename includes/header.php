7be2549a58f17dc516370c6308d0b15760a0e6d8
Jitendra <nerdcode@Jitendras-MacBook-Air.local>
2026-02-02 17:32:46 +0530

    correcting services spelling

--- includes/header.php
+++ includes/header.php
@@ -41,7 +41,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav m-auto">
                 <li class="nav-item">
-                  <a class="nav-link  active"  href="service.php">Servics <span>[6]</span></a>
+                  <a class="nav-link  active"  href="service.php">Services <span>[6]</span></a>
                 </li>
                 <li class="nav-item">
                   <a class="nav-link" href="aboutus.php">About</a>
