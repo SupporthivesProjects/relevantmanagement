0fcf3206b3a6c9bd19c0f8da9dfdf0ae41d7f570
SUPPORT HIVES <supporthives@SUPPORTs-MacBook-Air.local>
2026-02-06 19:14:21 +0530

    service page

--- contact_handler.php
+++ contact_handler.php
@@ -1,5 +1,5 @@
 <?php
-// require_once 'config.php';
+require_once 'config.php';
 require_once 'PHPMailer/src/Exception.php';
 require_once 'PHPMailer/src/PHPMailer.php';
 require_once 'PHPMailer/src/SMTP.php';
